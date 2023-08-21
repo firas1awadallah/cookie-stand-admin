762a05f01c96c5353efd37f2c4641b102184beed
firas1awadallah <firasawa@gmail.com>
2023-08-21 19:40:06 +0000

    Version2

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="p-6 text-center text-white bg-blue-400">
+      <h1 className="text-5xl font-semibold">Cookie Stand Admin</h1>
+      <p className="mt-2 text-lg">Manage your cookie stands with ease!</p>
+    </header>
+  );
+}
